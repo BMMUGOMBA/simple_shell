11636ceafbaa3b5babb028544f23d5f1973285f3
beemusy <beemusy@gmail.com>
2023-04-26 14:20:28 +0200

    biltin functions

--- biltin.c
+++ biltin.c
@@ -5,3 +5,13 @@ int (*builtin_func[]) (char **) = {
 &sys_chdir,
 &sys_exit,
 };
+
+
+/**
+* sys_num_builtins - system builtin fxns
+* Return: return hshs
+*/
+int sys_num_builtins(void)
+{
+return (sizeof(builtin_str) / sizeof(char *));
+}
