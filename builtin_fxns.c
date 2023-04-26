76ba9856e754c7a9cea9211acd7331e3a055f6a2
beemusy <beemusy@gmail.com>
2023-04-26 14:30:35 +0200

    builtin

--- builtin_fxns.c
+++ builtin_fxns.c
@@ -24,3 +24,36 @@ int printenv(char *copy_env[])
  * @args: arguments
  * Return: 1 or 0
 */
+int sys_chdir(char **args)
+{
+if (args[1] == NULL)
+{
+if (chdir("/home") != 0)
+perror("chdir() to /home failed");
+}
+else
+{
+if (chdir(args[1]) != 0)
+{
+perror("shell");
+}
+}
+return (1);
+}
+
+
+
+/**
+*sys_exit - Builtin command: exit.
+*@args: List of args.  Not examined.
+*Return: Always returns 0, to terminate execution.
+*/
+int sys_exit(char **args)
+{
+	if (args[1] == NULL)
+	{
+	return (0);
+	}
+	else
+	return (0);
+}
