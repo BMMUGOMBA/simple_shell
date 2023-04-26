de890741a0287059ddfceaad5547b433c9b507b1
beemusy <beemusy@gmail.com>
2023-04-26 15:09:25 +0200

    header file

--- main.h
+++ main.h
@@ -13,4 +13,20 @@
 
 /*
 * Function Declarations for shell commands:
- */
+*/
+int sys_chdir(char **args);
+int sys_help(void);
+int sys_exit(char **args);
+int printenv(char *copy_env[]);
+char *get_location(char *command);
+int sys_launch(char **args);
+int sys_execute(char **args, char *copy_env[], char *builtin_str[]);
+char *get_input(void);
+char **input_passer(char *line);
+void main_loop(char *copy_env[]);
+char *builtin_str[2];
+int (*builtin_func[2]) (char **);
+int sys_num_builtins(void);
+
+
+#endif /* MAIN_H */
