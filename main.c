7989e43aae626d19b89824c70e687a36faed30a3
beemusy <beemusy@gmail.com>
2023-04-26 14:53:17 +0200

    main fun

--- main.c
+++ main.c
@@ -7,3 +7,28 @@
  * @envp: envp param
  * Return: EXIT_SUCCESS
  */
+int main(int argc, char *argv[], char *envp[])
+{
+int i = 0;
+
+	char *copy_env[100];
+	int a;
+
+	a = 0;
+
+	a = a + argc;
+	argc = 0;
+	argv[1] = 0;
+	while (envp[i] != NULL)
+	{
+    /*printf("%s\n", envp[i]);*/
+	copy_env[i] = envp[i];
+	argv[2] = copy_env[1];
+	i++;
+	}
+
+	main_loop(copy_env);
+
+
+	return (EXIT_SUCCESS);
+}
