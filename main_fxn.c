1a248d029212ccbe7583ccf5451aba917d6217f5
beemusy <beemusy@gmail.com>
2023-04-26 15:15:38 +0200

    main_fxn.c

--- main_fxn.c
+++ main_fxn.c
@@ -79,3 +79,73 @@ return ((*builtin_func[i])(args));
 
 return (sys_launch(args));
 }
+/**
+* get_input - read input
+* Return: line
+*/
+char *get_input(void)
+{
+#ifdef LSH_USE_STD_GETLINE
+char *line = NULL;
+ssize_t bufsize = 0;
+if (getline(&line, &bufsize, stdin) == -1)
+if (feof(stdin))
+{
+exit(EXIT_SUCCESS);
+}
+else
+{
+perror("lsh: getline\n");
+exit(EXIT_FAILURE);
+}
+return (line);
+}
+/**
+ * get_input2 - continues input 1
+ * line over 40 lines
+ * @line: input line
+ * Return: 1 or 0
+*/
+char *get_input2(char *line)
+{
+#else
+#define LSH_RL_BUFSIZE 1024
+int bufsize = LSH_RL_BUFSIZE;
+int position = 0;
+char *buffer = malloc(sizeof(char) * bufsize);
+int c;
+if (!buffer)
+{
+fprintf(stderr, "lsh: allocation error\n");
+exit(EXIT_FAILURE);
+}
+while (1)
+{
+c = getchar();
+if (c == EOF)
+{
+exit(EXIT_SUCCESS);
+}
+else if (c == '\n')
+{
+buffer[position] = '\0';
+return (buffer);
+}
+else
+{
+buffer[position] = c;
+}
+position++;
+if (position >= bufsize)
+{
+bufsize += LSH_RL_BUFSIZE;
+buffer = realloc(buffer, bufsize);
+if (!buffer)
+{
+fprintf(stderr, "lsh: allocation error\n");
+exit(EXIT_FAILURE);
+}
+}
+}
+}
+#endif
