5432fd36d6b9a28cd57aabfc4088b1acfec5873f
Prof BATSI <34034828+BMMUGOMBA@users.noreply.github.com>
2023-04-26 14:33:51 +0200

    Update get_location.c

---
+++ get_location.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_location - gets the path for env variables
+ * @command: input
+ * Return: get
+*/
+char *get_location(char *command)
+{
+char *env, *env_copy, *env_token, *file_env;
+int cmd_len, dir_len;
+struct stat buffer;
+
+env = getenv("PATH");
+if (env)
+{
+env_copy = strdup(env);
+cmd_len = strlen(command);
+env_token = strtok(env_copy, ":");
+while (env_token != NULL)
+{
+dir_len = strlen(env_token);
+file_env = malloc(cmd_len + dir_len + 2);
+strcpy(file_env, env_token);
+strcat(file_env, "/");
+strcat(file_env, command);
+strcat(file_env, "\0");
