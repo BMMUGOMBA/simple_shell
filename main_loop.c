d781081bccfdca9b92c954925885cf9c6971acfe
Prof BATSI <34034828+BMMUGOMBA@users.noreply.github.com>
2023-04-26 15:20:22 +0200

    main_loop.c

--- main_loop.c
+++ main_loop.c
@@ -1,1 +1,16 @@
+#include "main.h"
+
+/**
+ * main_loop - main executing loop
+ * @copy_env: environment varible
+*/
+void main_loop(char *copy_env[])
+{
+char *line;
+char **args;
+int status;
+char *builtin_str[] = {
+"cd",
+"exit"
+};
 
