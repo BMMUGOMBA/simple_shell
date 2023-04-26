11c8cb0f6325408908f272f9d229c1b04f6471f5
beemusy <beemusy@gmail.com>
2023-04-26 14:35:22 +0200

    get_location.c

--- get_location.c
+++ get_location.c
@@ -25,3 +25,26 @@ strcpy(file_env, env_token);
 strcat(file_env, "/");
 strcat(file_env, command);
 strcat(file_env, "\0");
+if (stat(file_env, &buffer) == 0)
+{
+free(env_copy);
+
+return (file_env);
+}
+else
+{
+free(file_env);
+env_token = strtok(NULL, ":");
+
+}
+}
+
+free(env_copy);
+if (stat(command, &buffer) == 0)
+{
+return (command);
+}
+return (NULL);
+}
+return (NULL);
+
