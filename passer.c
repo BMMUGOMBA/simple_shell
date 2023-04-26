f1c4ddd62973d11c3d8b3e5601b256c72e8a7790
Prof BATSI <34034828+BMMUGOMBA@users.noreply.github.com>
2023-04-26 15:26:24 +0200

    passer.c 1st update

--- passer.c
+++ passer.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+/**
+ * input_passer - slpit input into tokens
+ * @line: line input
+ * Return: 1 or 0
+*/
+char **input_passer(char *line)
+{
+int bufsize = LSH_TOK_BUFSIZE, position = 0;
+char **tokens = malloc(bufsize * sizeof(char *));
+char *token, **tokens_backup;
+
+if (!tokens)
+{
+fprintf(stderr, "lsh: allocation error\n");
+exit(EXIT_FAILURE);
+}
