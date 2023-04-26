60104e564eb39901157977efafdf98726102bf22
Prof BATSI <34034828+BMMUGOMBA@users.noreply.github.com>
2023-04-26 15:08:02 +0200

    main.h update 1

--- main.h
+++ main.h
@@ -1,1 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
+
+
+/*
+* Function Declarations for shell commands:
+ */
