51438332cb178af2faeef7dbceb79ee3e0f0f7c4
beemusy <67947204+beemusy@users.noreply.github.com>
2023-04-27 03:38:42 +0200

    update utlity fx bmoyo

--- utility_fxns.c
+++ utility_fxns.c
@@ -17,3 +17,28 @@ void SignalHandler(int error)
 		break;
 	}
 }
+/**
+ * intmodestatus -  doesn`t open prompt and executes one time
+ * @line: Typed command by user sent to splitline
+ * @argc: Splitted command sent or not n
+ * Return: 1 if completed 0 if failed
+**/
+void intmodestatus(char *line, char **argc)
+{
+	int addr;
+	int modeStatus = 1;
+	size_t len = 0;
+
+	while (getline(&line, &len, stdin) != EOF)
+	{
+		argc = _passinput(line);
+		if (argc[0] != NULL)
+			modeStatus = _launch(argc, &addr, 1);
+		if (modeStatus == 0)
+			if (addr == 1)
+				free(argc[0]);
+		free(argc);
+	}
+	free(line);
+}
+
