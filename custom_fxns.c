419dec8206ff647c71fe39aed4eb12c1448cf803
Prof BATSI <34034828+BMMUGOMBA@users.noreply.github.com>
2023-04-27 03:42:46 +0200

    custom fxn 2nd edits

--- custom_fxns.c
+++ custom_fxns.c
@@ -19,3 +19,34 @@ char *_getline(void)
 	write(1, "\n", 1);
 	return (NULL);
 }
+
+/**
+ * _strtok - tokenizes a string
+ * @s: string to be tokenized
+ * @deli: delimitator for the tokens
+ * Return: pointer to each token
+ */
+char *_strtok(char *s, char *deli)
+{
+	int c;
+	static char *str;
+
+	if (!s)
+		s = str;
+	c = *s++;
+	while (strchr(deli, c))
+	{
+		if (c == 0)
+			return (0);
+		c = *s++;
+	}
+	--s;
+	str = s + strcspn(s, deli);
+	if (*str != 0)
+	{
+		*str = 0;
+		str++;
+	}
+	return (s);
+}
+
