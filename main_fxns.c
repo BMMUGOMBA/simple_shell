adf2b8376a97bcb8ccf52705863269425488317d
beemusy <67947204+beemusy@users.noreply.github.com>
2023-04-27 03:39:58 +0200

    update main_fxns 2nd commit

--- main_fxns.c
+++ main_fxns.c
@@ -117,3 +117,79 @@ return (resultado);
  * @mode:Flag to select the mode, interactive or inninterative
  * Return: Returns the unconcatenated path for execution
 **/
+
+
+char *_getlocation(char *command, int mode)
+{
+	int i = 0, j = 0, flagcurr = 0;
+	char *completepath = NULL, *envcpy = NULL, *token = NULL;
+	char **tokens = NULL;
+
+	tokens = (char **)malloc(sizeof(char *) * 1024);
+	if (tokens == NULL)
+		return (NULL);
+
+	while (environ[i])
+	{
+		if (_strcmp_path(environ[i], "PATH=") == 0)
+			break;
+		i++;
+	}
+	envcpy = malloc(sizeof(char) * strlen(environ[i]) + 1);
+	if (envcpy == NULL)
+		return (NULL);
+
+	strcpy(envcpy, environ[i]);
+	token = _strtok(envcpy, "=\n\r");
+	if (environ[i][5] == ':')
+		flagcurr = 1;
+	while (token != NULL)
+	{
+		tokens[j] = token;
+		token = _strtok(NULL, ":\n\r");
+		j++;
+	}
+	if (flagcurr == 1)
+		tokens[0] = "./";
+
+	completepath = _catpath(tokens, command);
+	free(tokens);
+	free(token);
+	free(envcpy);
+	if (mode == 0)
+		free(command);
+
+return (completepath);
+}
+
+/**
+ *  _catpath - Concatenates path of command.
+ * @uncatpath: Uncocatenated path
+ * @command: Command issued by the user
+ * Return: Returns the concatenated path for execution
+**/
+char *_catpath(char **uncatpath, char *command)
+{
+	int i = 0;
+	char *path = NULL;
+
+	path = malloc(sizeof(char) * 1024);
+	if (path == NULL)
+		return (NULL);
+
+	for (i = 0; i < 1024; i++)
+		path[i] = '\0';
+
+	i = 0;
+	while (uncatpath[i] != NULL)
+	{
+		strcpy(path, uncatpath[i]);
+		strcat(path, "/");
+		strcat(path, command);
+		if (access(path, F_OK & X_OK) != -1)
+			break;
+		i++;
+	}
+return (path);
+}
+
